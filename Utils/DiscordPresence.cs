77e24bcef621833041e07142c16e25b14dba4ba6
yoqzii <109358690+ignYoqzii@users.noreply.github.com>
2023-02-05 16:41:19 -0500

    Add files via upload

---
+++ Utils/DiscordPresence.cs
@@ -0,0 +1,34 @@
+﻿using DiscordRPC;
+
+namespace StarZLauncher.Utils;
+
+public static class DiscordPresence
+{
+    public static readonly DiscordRpcClient DiscordClient = new("1071616808752259164");
+    
+    public static void IdlePresence()
+    {
+        DiscordClient.SetPresence(new RichPresence
+        {
+            State = "Idling in the client",
+            Timestamps = Timestamps.Now,
+            Buttons = new[]
+            {
+                new Button { Label = "Download StarZ Launcher", Url = "https://cdn.discordapp.com/attachments/" }
+            },
+            Assets = new Assets
+            {
+                LargeImageKey = "starz",
+                LargeImageText = "StarZ Launcher Icon"
+            }
+        });
+    }
+    
+    public static void StopPresence()
+    {
+        if (DiscordClient.IsDisposed) return;
+        DiscordClient.ClearPresence();
+        DiscordClient.Deinitialize();
+        DiscordClient.Dispose();
+    }
+}
